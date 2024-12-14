d60497efb1150f0d2a7c8a4c6b351ae1a4588a2d
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-12-14 18:55:12 +0300

    Ürünleri görüntüleme sayfası yapıldı

--- MarketTracking/products/views.py
+++ MarketTracking/products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from anasayfa.models import Products
 # Create your views here.
 
 def show_products(request):
-    return render(request, "products/goruntule.html")
+    product_list = Products.objects.all()
+
+    return render(request, "products/goruntule.html", {'product_list': product_list})
